47b35f95b708add631d5d75a556216fdd4abc1d7
dbcometto <111076949+dbcometto@users.noreply.github.com>
2025-05-01 06:40:20 -0600

    radio progress

--- fmcw_sim.py
+++ fmcw_sim.py
@@ -11,7 +11,6 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from PyQt5 import QtCore
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import fft
@@ -24,6 +23,8 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
 import numpy as np
 import sip
 
@@ -64,26 +65,54 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 80000000
-        self.t_chirp = t_chirp = 0.00005
-        self.min_freq = min_freq = 0
-        self.max_freq = max_freq = samp_rate/2
-        self.distance_m = distance_m = 100
+        self.samp_rate = samp_rate = int(7e6)
+        self.max_freq = max_freq = samp_rate*0.3
+        self.t_chirp = t_chirp = 2
+        self.min_freq = min_freq = -max_freq
+        self.k_bins = k_bins = 2**18
         self.c = c = 300000000
-        self.k_bins = k_bins = 2**14
-        self.delay_time = delay_time = 2*distance_m/c
-        self.alpha = alpha = (max_freq-min_freq)/t_chirp
-        self.reflection_amplitude = reflection_amplitude = 0.75
-        self.f_b_ideal = f_b_ideal = 2*distance_m*alpha/c
-        self.distance_per_sample = distance_per_sample = c/samp_rate
-        self.distance_max_m = distance_max_m = 200
+        self.system_delay = system_delay = 0
+        self.radio_tx_gain = radio_tx_gain = 40
+        self.radio_rx_gain = radio_rx_gain = 50
+        self.radio_center = radio_center = 1e9
+        self.distance_per_sample = distance_per_sample = c/samp_rate/2
+        self.distance_max_m = distance_max_m = 100
         self.df = df = samp_rate/k_bins
-        self.delay_samples = delay_samples = int(delay_time*samp_rate)
+        self.alpha = alpha = (max_freq-min_freq)/t_chirp
 
         ##################################################
         # Blocks
         ##################################################
 
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_source_0.set_center_freq(radio_center, 0)
+        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self.uhd_usrp_source_0.set_gain(radio_rx_gain, 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0.set_center_freq(radio_center, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_gain(radio_tx_gain, 0)
         self.qtgui_number_sink_0_1_0 = qtgui.number_sink(
             gr.sizeof_float,
             0,
@@ -183,6 +212,48 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self.qtgui_number_sink_0.enable_autoscale(False)
         self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_number_sink_0_win)
+        self.qtgui_freq_sink_x_0_0_0 = qtgui.freq_sink_c(
+            2048, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "TxRx Signal", #name
+            2,
+            None # parent
+        )
+        self.qtgui_freq_sink_x_0_0_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0_0_0.set_y_axis((-140), 10)
+        self.qtgui_freq_sink_x_0_0_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0_0_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0_0_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0_0_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0_0_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0_0_0.enable_control_panel(True)
+        self.qtgui_freq_sink_x_0_0_0.set_fft_window_normalized(False)
+
+
+
+        labels = ["Rx", 'Tx', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0_0_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0_0_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0_0_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_0_win)
         self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
             2048, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -226,20 +297,16 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
         self.fft_vxx_0_0 = fft.fft_vcc(k_bins, True, window.blackmanharris(k_bins), False, 2)
-        self._distance_m_range = qtgui.Range(0, distance_max_m, 1, 100, 200)
-        self._distance_m_win = qtgui.RangeWidget(self._distance_m_range, self.set_distance_m, "Object Distance", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._distance_m_win)
         self.blocks_vco_c_0 = blocks.vco_c(samp_rate, (2*np.pi), 1)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_stream_to_vector_0_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, k_bins)
         self.blocks_short_to_float_0 = blocks.short_to_float(1, 1)
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_short*1)
-        self.blocks_multiply_const_vxx_2 = blocks.multiply_const_cc(reflection_amplitude)
         self.blocks_multiply_const_vxx_1_0 = blocks.multiply_const_ff((c/2/alpha))
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_ff((1*df))
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(((max_freq-min_freq)))
         self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
-        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, delay_samples)
+        self.blocks_delay_0_0 = blocks.delay(gr.sizeof_float*1, (samp_rate*3))
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, system_delay)
         self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(k_bins)
         self.blocks_argmax_xx_1 = blocks.argmax_fs(k_bins)
         self.blocks_add_const_vxx_0 = blocks.add_const_ff(min_freq)
@@ -249,26 +316,28 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_delay_0_0, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_vco_c_0, 0))
         self.connect((self.blocks_argmax_xx_1, 1), (self.blocks_null_sink_0, 0))
         self.connect((self.blocks_argmax_xx_1, 0), (self.blocks_short_to_float_0, 0))
         self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_argmax_xx_1, 0))
-        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_const_vxx_2, 0))
+        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
+        self.connect((self.blocks_delay_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.blocks_stream_to_vector_0_0, 0))
         self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.blocks_multiply_const_vxx_1_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_number_sink_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1_0, 0), (self.qtgui_number_sink_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_2, 0), (self.blocks_multiply_conjugate_cc_0, 1))
         self.connect((self.blocks_short_to_float_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.blocks_short_to_float_0, 0), (self.qtgui_number_sink_0_1_0, 0))
         self.connect((self.blocks_stream_to_vector_0_0, 0), (self.fft_vxx_0_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_vco_c_0, 0), (self.blocks_delay_0, 0))
-        self.connect((self.blocks_vco_c_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
+        self.connect((self.blocks_vco_c_0, 0), (self.qtgui_freq_sink_x_0_0_0, 1))
+        self.connect((self.blocks_vco_c_0, 0), (self.uhd_usrp_sink_0, 0))
         self.connect((self.fft_vxx_0_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -284,13 +353,24 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_delay_samples(int(self.delay_time*self.samp_rate))
         self.set_df(self.samp_rate/self.k_bins)
-        self.set_distance_per_sample(self.c/self.samp_rate)
-        self.set_max_freq(self.samp_rate/2)
+        self.set_distance_per_sample(self.c/self.samp_rate/2)
+        self.set_max_freq(self.samp_rate*0.3)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.blocks_delay_0_0.set_dly(int((self.samp_rate*3)))
         self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_freq_sink_x_0_0_0.set_frequency_range(0, self.samp_rate)
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+
+    def get_max_freq(self):
+        return self.max_freq
+
+    def set_max_freq(self, max_freq):
+        self.max_freq = max_freq
+        self.set_alpha((self.max_freq-self.min_freq)/self.t_chirp)
+        self.set_min_freq(-self.max_freq)
+        self.blocks_multiply_const_vxx_0.set_k(((self.max_freq-self.min_freq)))
 
     def get_t_chirp(self):
         return self.t_chirp
@@ -309,32 +389,6 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self.blocks_add_const_vxx_0.set_k(self.min_freq)
         self.blocks_multiply_const_vxx_0.set_k(((self.max_freq-self.min_freq)))
 
-    def get_max_freq(self):
-        return self.max_freq
-
-    def set_max_freq(self, max_freq):
-        self.max_freq = max_freq
-        self.set_alpha((self.max_freq-self.min_freq)/self.t_chirp)
-        self.blocks_multiply_const_vxx_0.set_k(((self.max_freq-self.min_freq)))
-
-    def get_distance_m(self):
-        return self.distance_m
-
-    def set_distance_m(self, distance_m):
-        self.distance_m = distance_m
-        self.set_delay_time(2*self.distance_m/self.c)
-        self.set_f_b_ideal(2*self.distance_m*self.alpha/self.c)
-
-    def get_c(self):
-        return self.c
-
-    def set_c(self, c):
-        self.c = c
-        self.set_delay_time(2*self.distance_m/self.c)
-        self.set_distance_per_sample(self.c/self.samp_rate)
-        self.set_f_b_ideal(2*self.distance_m*self.alpha/self.c)
-        self.blocks_multiply_const_vxx_1_0.set_k((self.c/2/self.alpha))
-
     def get_k_bins(self):
         return self.k_bins
 
@@ -342,33 +396,42 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self.k_bins = k_bins
         self.set_df(self.samp_rate/self.k_bins)
 
-    def get_delay_time(self):
-        return self.delay_time
+    def get_c(self):
+        return self.c
 
-    def set_delay_time(self, delay_time):
-        self.delay_time = delay_time
-        self.set_delay_samples(int(self.delay_time*self.samp_rate))
-
-    def get_alpha(self):
-        return self.alpha
-
-    def set_alpha(self, alpha):
-        self.alpha = alpha
-        self.set_f_b_ideal(2*self.distance_m*self.alpha/self.c)
+    def set_c(self, c):
+        self.c = c
+        self.set_distance_per_sample(self.c/self.samp_rate/2)
         self.blocks_multiply_const_vxx_1_0.set_k((self.c/2/self.alpha))
 
-    def get_reflection_amplitude(self):
-        return self.reflection_amplitude
+    def get_system_delay(self):
+        return self.system_delay
 
-    def set_reflection_amplitude(self, reflection_amplitude):
-        self.reflection_amplitude = reflection_amplitude
-        self.blocks_multiply_const_vxx_2.set_k(self.reflection_amplitude)
+    def set_system_delay(self, system_delay):
+        self.system_delay = system_delay
+        self.blocks_delay_0.set_dly(int(self.system_delay))
 
-    def get_f_b_ideal(self):
-        return self.f_b_ideal
+    def get_radio_tx_gain(self):
+        return self.radio_tx_gain
 
-    def set_f_b_ideal(self, f_b_ideal):
-        self.f_b_ideal = f_b_ideal
+    def set_radio_tx_gain(self, radio_tx_gain):
+        self.radio_tx_gain = radio_tx_gain
+        self.uhd_usrp_sink_0.set_gain(self.radio_tx_gain, 0)
+
+    def get_radio_rx_gain(self):
+        return self.radio_rx_gain
+
+    def set_radio_rx_gain(self, radio_rx_gain):
+        self.radio_rx_gain = radio_rx_gain
+        self.uhd_usrp_source_0.set_gain(self.radio_rx_gain, 0)
+
+    def get_radio_center(self):
+        return self.radio_center
+
+    def set_radio_center(self, radio_center):
+        self.radio_center = radio_center
+        self.uhd_usrp_sink_0.set_center_freq(self.radio_center, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.radio_center, 0)
 
     def get_distance_per_sample(self):
         return self.distance_per_sample
@@ -389,12 +452,12 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self.df = df
         self.blocks_multiply_const_vxx_1.set_k((1*self.df))
 
-    def get_delay_samples(self):
-        return self.delay_samples
+    def get_alpha(self):
+        return self.alpha
 
-    def set_delay_samples(self, delay_samples):
-        self.delay_samples = delay_samples
-        self.blocks_delay_0.set_dly(int(self.delay_samples))
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+        self.blocks_multiply_const_vxx_1_0.set_k((self.c/2/self.alpha))
 
 
 
